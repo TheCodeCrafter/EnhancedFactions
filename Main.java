97a6a561a25f3822da6abdaf1c01462d88465583
Noah <noahcoetsee@gmail.com>
2018-02-06 14:32:57 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,51 @@
 package net.fathomtech.plugins.EnhancedFactions;
 
 public class Main extends JavaPlugin {
+  
   FocusFile focus;
+  HashMap<Player, ArrayList<Player>> cache = new HashMap<Player, ArrayList<Player>>();
   
   @Override
   public void onEnable() {
     saveDefaultConfig();
     focus = new FocusFile(this);
+    
+    ArrayList<String> focusedPlayers = (ArrayList<String>) focus.data.getKeys(false);
+    for(String playername : focusedPlayers) {
+      focusPlayer
+    }
   }
   
   @Override
   public void onDisable() {
     saveConfig();
     focus.save();
+  }
+  
+  public void addFocusedPlayer(FPlayer target, Faction faction) {
+    Set<FPlayer> members = faction.getFPlayersWhereOnline(true);
+    faction.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "" + target.getName() + " has been focused!");
+    focus.data.setList(target.getName() + ".recipients", members);
+    this.focusPlayer();
+  }
+  
+  public void addFocusedPlayer(FPlayer target, Faction faction, FPlayer sender, String message) {
+    Set<FPlayer> members = faction.getFPlayersWhereOnline(true);
+    faction.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "" + target.getName() + " has been focused!");
+    faction.addAnnouncement(sender, message);
+    focus.data.setList(target.getName() + ".recipients", members);
+    this.focusPlayer();
+  }
+  
+  private void focusPlayer(FPlayer target, Set<FPlayer> recipients) {
+    for(FPlayer player : recipients) {
+      changeNameTag(player, target, getConfig().getString("focusedPrefix"), getConfig().getString("focusedSuffix"), TeamAction.CREATE);
+    }
+  }
+  
+  private void unfocusPlayer(FPlayer target, Set<FPlayer> recipients) {
+    for(FPlayer player : recipients) {
+      changeNameTag(player, target, "", "", TeamAction.DESTROY);
+    }
   }
 }
