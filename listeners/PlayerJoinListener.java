6aa4a420b6216bbcbf9530fc636ef590814f28fe
Noah <noahcoetsee@gmail.com>
2018-02-06 20:36:29 -0500

    Update PlayerJoinListener.java

--- listeners/PlayerJoinListener.java
+++ listeners/PlayerJoinListener.java
@@ -1,5 +1,31 @@
 package net.fathomtech.plugins.EnhancedFactions.listeners;
 
 public class PlayerJoinListener implements Listener {
-
+  @EventHandler
+  public void isPlayerFocusedOnJoin(PlayerJoinEvent event) {
+    Player player = event.getPlayer();
+    
+    Player focusedPlayer = null;
+    
+    // GET DATA
+    Set<String> focusedPlayers = this.plugin.focus.data.getKeys(false);
+    
+    // CHECK IF PLAYER IS FOCUSED
+    for(String name : focusedPlayers) {
+      if(name.equalsIgnoreCase(player.getName())) {
+        Set<FPlayers> recipients = (Set<FPlayers>) this.plugin.focus.data.getSet(name + ".recipients");
+        this.plugin.focusPlayer(FPlayers.getInstance().getByPlayer(player), recipients);
+      }
+    }
+         
+  }
+  
+  @EventHandler
+  public void isPlayerRecipientFocusedPlayer(PlayerJoinEvent event) {
+    Player player = event.getPlayer();
+    
+    // FIND OUT WHO IS FOCUSED
+    
+    // SEND PACKET TO RE-FOCUS ON THAT PLAYER
+  }
 }
