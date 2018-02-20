e9a229766d3dc6cadc64bbf301524bf9be7c31ff
Noah <noahcoetsee@gmail.com>
2018-02-20 10:15:29 -0500

    Made basic conditions

--- commands/UpgradeCommand.java
+++ commands/UpgradeCommand.java
@@ -1,5 +1,9 @@
 package net.fathomtech.plugins.EnhancedFactions.commands;
 
 public class UpgradeCommand implements CommandExecutor {
-  
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if(!(sender instanceof Player)) {
+      sender.sendMessage("You must be a player and in a faction to use this command!");
+    }
+  }
 }
