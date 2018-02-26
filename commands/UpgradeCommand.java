62ce0b8ee768ba756a14859cc1c3af27d524957c
Noah <noahcoetsee@gmail.com>
2018-02-26 09:45:12 -0500

    Added faction hook-ins

--- commands/UpgradeCommand.java
+++ commands/UpgradeCommand.java
@@ -5,5 +5,18 @@ public class UpgradeCommand implements CommandExecutor {
     if(!(sender instanceof Player)) {
       sender.sendMessage("You must be a player and in a faction to use this command!");
     }
+    
+    
+    Player player = (Player) sender;
+    FPlayer factionPlayer = FPlayers.getByPlayer(player);
+    Faction faction = factionPlayer.getFaction();
+    
+    if(faction == null || faction.isWilderness()) {
+      factionPlayer.sendMessage("You must be in a faction to use this command!");
+      return true;
+    }
+    
+    // Since we now know they're in a faction, let's check to see if they have permission
+    
   }
 }
